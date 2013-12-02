a67a41d0af7c320c100dac749135e93a147c90b3
Nathaniel Watts <reg@nathanielwatts.com>
2013-12-01 18:50:58 -0700

    remove index/show actions and specs for phone/email

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 ContactManager::Application.routes.draw do
   resources :companies
 
-  resources :email_addresses
+  resources :email_addresses, :except => [:index, :show]
 
-  resources :phone_numbers
+  resources :phone_numbers, :except => [:index, :show]
 
   resources :people
 
